33ed02d44ed4c428ecd2f4c880443c1e1cb97737
JP Given <jgiven@gmail.com>
2017-12-08 12:49:44 -0800

    Beginning Vue Component

---
+++ vitruvius-framework/components/buttons/vuejs/MoxiButton.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    props: [
+      'label',
+      'icon',
+      'classes'
+    ]
+  }
+</script>
+
+<style>
+
+</style>
