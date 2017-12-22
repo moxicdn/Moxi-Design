a0919d0320fa735aff6b2de1a31106a33b05947e
dmartinka <34174110+dmartinka@users.noreply.github.com>
2017-12-22 15:11:01 -0800

    Removing projec to get the new...

--- vitruvius-framework/src/js/main.js
+++
@@ -1,9 +0,0 @@
-// Import WebSite Assets - Not included in build.
-// --------------------------------------------------
-import '../assets/animated-text.css'
-import '../assets/site.css'
-import '../assets/animated-text.js'
-
-// Import SCSS
-// --------------------------------------------------
-import '../scss/main.scss'
