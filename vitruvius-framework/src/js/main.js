86eca16d6335a83aea767a3f76b852e4f3fc6198
dmartinka <34174110+dmartinka@users.noreply.github.com>
2017-12-22 15:20:28 -0800

    New awesomeness

---
+++ vitruvius-framework/src/js/main.js
@@ -0,0 +1,9 @@
+// Import WebSite Assets - Not included in build.
+// --------------------------------------------------
+import '../assets/animated-text.css'
+import '../assets/site.css'
+import '../assets/animated-text.js'
+
+// Import SCSS
+// --------------------------------------------------
+import '../scss/main.scss'
